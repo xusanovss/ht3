a674a69ae11bcc93afcd5d97cb57986b0c64f2c6
xusanovss <khusanovotabek03@gmail.com>
2024-10-21 10:37:33 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class Cat:
+    def __init__(self, nick: str, age: int):
+        self.nickname = nick
+        self.age = age
+
+    def make_sound(self):
+        print(f"{self.nickname}: Meow!")
+
---
+++ sinf ishi.py
@@ -0,0 +1,35 @@
+#1 vazifa
+class Odam:
+    def __init__(self, ism):
+        self.ism = ism
+
+    def salomlashish(self):
+        print(f"Salom {self.ism}")
+
+ism = input("Ismingizni kiriting: ")
+
+odam = Odam(ism)
+
+odam.salomlashish()
+
+#2 vazifa
+class Odam:
+    def __init__(self, ism):
+        self.ism = ism
+
+    def kuylash(self):
+        print(f"{self.ism} kuylamoqda... 🎶")
+
+    def eshitish(self):
+        print(f"{self.ism} kuy eshitmoqda...")
+
+    def gapirish(self, boshqa_ism):
+        print(f"{self.ism}: Juda yaxshi kuylading, {boshqa_ism}!")
+
+odam1 = Odam("Azamjon")
+odam2 = Odam("Javlonbek")
+
+odam1.kuylash()
+
+odam2.eshitish()
+odam2.gapirish(odam1.ism)
